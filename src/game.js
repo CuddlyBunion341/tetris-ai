4d4b2b61e8a4af3ea06875ee60f7a0254e8ccc76
CuddlyBunion341 <daniel@bengl.cz>
2023-04-06 22:05:35 +0200

    added js piece queues and game over

--- src/game.js
+++ src/game.js
@@ -5,65 +5,90 @@ class Game {
 	currentShape = null;
 	active = true;
 	shapes = [];
+	queue = [];
 
 	static loop() {
 		Game.games.forEach((game, i) => {
-			if (i !== 0) {
+			if (false) {
+				// do not use random movement for now
 				const num = Math.floor(Math.random() * 5);
 				if (num === 0) game.moveLeft();
 				if (num === 1) game.moveRight();
 				if (num === 2) game.moveDown();
 				if (num === 3) game.drop();
 			}
-			if (game.active) {
-				game.nextStep();
-			}
+
+			game.nextStep();
 		});
 
 		setTimeout(Game.loop, 500);
 	}
 
-	constructor(gameBoard) {
+	constructor(board) {
 		Game.games.push(this);
-		this.gameBoard = gameBoard;
+		this.board = board;
 
 		this.shapes = ShapeTemplate.shapes.map(
 			(shape) => new Shape(shape, this)
 		);
 
+		// fill queue
+		for (let i = 0; i < 3; i++) {
+			this.queue.push(this.randomShape());
+		}
+
 		this.nextShape();
+	}
+
+	randomShape() {
+		return this.shapes[Math.floor(Math.random() * this.shapes.length)];
 	}
 
 	nextShape() {
 		this.currentShape?.reset();
-		const randomShape =
-			this.shapes[Math.floor(Math.random() * this.shapes.length)];
-		this.currentShape = randomShape;
+
+		this.currentShape = this.queue.shift();
+		this.queue.push(this.randomShape());
+
+		// test game over
+		if (this.currentShape.testCollision(0, 0)) {
+			this.stop();
+			return;
+		}
+
 		this.currentShape.place();
+
+		this.board.container.style.outlineColor = this.currentShape.color;
 	}
 
 	nextStep() {
+		if (!this.active) return;
 		this.currentShape.moveDown();
 	}
 
 	// movement
 	moveLeft() {
+		if (!this.active) return;
 		this.currentShape.moveLeft();
 	}
 
 	moveRight() {
+		if (!this.active) return;
 		this.currentShape.moveRight();
 	}
 
 	moveDown() {
+		if (!this.active) return;
 		this.currentShape.moveDown();
 	}
 
 	drop() {
+		if (!this.active) return;
 		this.currentShape.drop();
 	}
 
 	rotate() {
+		if (!this.active) return;
 		this.currentShape.rotate();
 	}
 
